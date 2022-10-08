decb3ea658b652791e9d94849a55908beb7aa1de
Michael Scott <61732981+sctmic015@users.noreply.github.com>
2022-10-08 15:22:36 +0200

    More Stuff and Started Report

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import gnb as gnb
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from collections import Counter
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+#import pyAgrum as gum
+#import pyAgrum.lib.notebook as gnb
+from sklearn.metrics import accuracy_score
+#import pyAgrum.skbn as skbn
+import pandas_profiling as pp
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas_profiling as pp
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data = pd.read_csv('heart_2020_cleaned.csv')
+data.head
 
+import phik
+from phik.report import plot_correlation_matrix
+from phik import report
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+phik_overview = data.phik_matrix()
+phik_overview.round(1)
+plot_correlation_matrix(phik_overview.values,
+                        x_labels=phik_overview.columns,
+                        y_labels=phik_overview.index,
+                        vmin=0, vmax=1,
+                        title=r"correlation $\phi_K$",
+                        fontsize_factor=0.5,
+                        figsize=(10, 8))
+plt.tight_layout()
+plt.savefig("corr.png")
